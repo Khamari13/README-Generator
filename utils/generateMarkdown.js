5e49cfe683728d677a6d72a3676e4a892cddfa7e
Khamari Thompson <61212885+Khamari13@users.noreply.github.com>
2020-05-23 21:04:00 -0400

    added MarkDown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,6 +3,55 @@ function generateMarkdown(data) {
 # ${data.title} ![License]
 (https://img.shields.io/badge/license-${data.license}-green)
 
+## Description
+
+${data.description}
+
+##Tablen of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install the dependencies, run the following command
+
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+## License
+
+This project uses ${data.license} license
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+The following command, runs the tests:
+
+\`\`\`
+${data.test}
+\`\`\`
+
+## Questions
+
+If you have any personal questions or ideas on how to improve this repo, contact [${data.github}](${data.url}) directly at ${data.email}.
 `;
 }
 
